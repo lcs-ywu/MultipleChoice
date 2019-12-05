0b6b8e5d911c22e176db8ef3ff0db0a8f9fad54b
James Wu <ywu@lcs.on.ca>
2019-12-04 21:56:25 -0500

    Finished connecting and all the guard statements

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    @IBOutlet weak var questionNumberInput: UITextField!
+    @IBOutlet weak var studentAnswerInput: UITextField!
+    @IBOutlet weak var correctAnswerInput: UITextField!
+    @IBOutlet weak var checkResult: UITextView!
+    
+    // MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func checkAnswers(_ sender: Any) {
+        guard let questionNumberAsString = questionNumberInput.text,questionNumberAsString.count>0 else {checkResult.text = "Please enter an integer value greater than 0"
+            return
+        }
+        guard let questionNumberAsInt = Int(questionNumberAsString)else{
+            checkResult.text = "Please enter an integer value greater than 0"
+            return
+        }
+        guard let studentAnswerAsString = studentAnswerInput.text,studentAnswerAsString.count == questionNumberAsInt
+            else{
+                checkResult.text = "Please be sure you input exactly \(questionNumberAsInt) student answers"
+                return
+        }
+        guard let correctAnswerAsString = correctAnswerInput.text,correctAnswerAsString.count == questionNumberAsInt else{
+            checkResult.text = "Please be sure you input exactly \(questionNumberAsInt) answers for the answer key"
+            return
+        }
+        let allPossibleAnswer = "ABCDE"
+        
+        for eachStudentAnswer in studentAnswerAsString {
+            
+            guard  allPossibleAnswer.contains(eachStudentAnswer) else{
+                checkResult.text = "Student answers contain invalid choices, please ensure only character A, B, C, D or E are used"
+                return
+            }
+            for eachCorrectAnswer in correctAnswerAsString {
+                guard allPossibleAnswer.contains(eachCorrectAnswer) else {
+                    checkResult.text = "Correct answers contain invalid choices, please ensure only character A, B, C, D or E are used"
+                    return
+                }
+                
+                
+                
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
 }
 
